51572031a4d6fd562281997a02249c776db56ba7
Asiyacm <asiyacmuhammed@gmail.com>
2022-11-25 01:24:17 +0530

    router commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,37 @@ import { SearchEmployeeComponent } from './search-employee/search-employee.compo
 import { ViewEmployeeComponent } from './view-employee/view-employee.component';
 import { DeleteEmployeeComponent } from './delete-employee/delete-employee.component';
 import { EditEmployeeComponent } from './edit-employee/edit-employee.component';
+import { RouterModule, Routes } from '@angular/router';
+
+
+
+const myRoute: Routes=[
+  {
+    path: "",
+    component:AdminLoginComponent
+  },
+  {
+    path: "add",
+    component:AddEmployeeComponent
+  },
+  {
+    path: "view",
+    component:ViewEmployeeComponent
+  },
+  {
+    path: "search",
+    component:SearchEmployeeComponent
+  },
+  {
+    path: "edit",
+    component:EditEmployeeComponent
+  },
+  {
+    path: "delete",
+    component:DeleteEmployeeComponent
+  },
+
+]
 
 @NgModule({
   declarations: [
@@ -22,7 +53,8 @@ import { EditEmployeeComponent } from './edit-employee/edit-employee.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
