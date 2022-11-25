bef7831e7c26c7840224b85bc85681df119d4439
Asiyacm <asiyacmuhammed@gmail.com>
2022-11-25 10:59:52 +0530

    data commit

--- src/app/view-employee/view-employee.component.ts
+++ src/app/view-employee/view-employee.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./view-employee.component.css']
 })
 export class ViewEmployeeComponent {
-
+  data:any =[
+    {
+  "name":"Asiya",
+  "designation":"software engineer trainee",
+  "salary":25000,
+  "company":"nest digital"
+  },
+  {
+    "name":"Anna",
+    "designation":"software engineer trainee",
+    "salary":25000,
+    "company":"nest digital"
+    },
+    {
+      "name":"Harsha",
+      "designation":"software engineer trainee",
+      "salary":25000,
+      "company":"nest digital"
+      },
+      {
+        "name":"Anjana",
+        "designation":"software engineer trainee",
+        "salary":25000,
+        "company":"nest digital"
+        },
+        {
+          "name":"Noora",
+          "designation":"software engineer trainee",
+          "salary":25000,
+          "company":"nest digital"
+          }
+]
 }
+
